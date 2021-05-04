a0f7379d7026f146fc062c7695cfdb1de6866a48
Nhicoulous <78459590+PsalmsGlobal@users.noreply.github.com>
2021-05-05 00:33:10 +0800

    updated

--- AccountFiles/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from AccountFiles.models import AccountFile
-# Register your models here.
-admin.site.register(AccountFile)
--- Authentication/views.py
+++ Authentication/views.py
@@ -1,8 +1,9 @@
-from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.views.generic import View
 from django.views import View
 from . views import*
+
 
 class LoginView(View):
     def get(self, request):
@@ -11,3 +12,6 @@ class LoginView(View):
 class RegisterView(View):
     def get(self, request):
         return render(request, 'authentication/register.html')
+
+
+
